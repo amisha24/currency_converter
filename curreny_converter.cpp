628105b47742554be8aad6f997b8b77f2f7bc1af
Amisha Sherekar <65601288+amisha24@users.noreply.github.com>
2021-03-06 22:43:41 +0530

    Add files via upload

---
+++ curreny_converter.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+  double pesos;
+  double reais;
+  double soles;
+
+  double dollars;
+  std::cout << "Enter number of Colombian Pesos:\n";
+ 
+  std::cin >> pesos;
+  
+  std::cout << "Enter number of Brazilian Reais:\n";
+
+  std::cin >> reais;
+
+  std::cout << "Enter number of Peruvian Soles:\n";
+  std::cin >> soles;
+
+  // Pesos conversion rate: 0.00032
+// Reais conversion rate: 0.27
+// Soles conversion rate: 0.3
+dollars = 0.00032 * pesos + 0.27 * reais + 0.3 * soles;
+std::cout << "Total USD = $" << dollars << "\n";
+
+
+
+
+
+
+}
